393ff52cbf8ca6c8ee1654e61ff81c0ac004ce00
Mathew Thompson <mathewthompson@Mathews-iMac.local>
2019-07-09 16:22:21 -0500

    First Commit

---
+++ app/app.js
@@ -0,0 +1,84 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+/*
+Video at: http://l.mat.fyi/rendering_06_light-paths-overview.mp4
+*/
+
+app.use( '/', (req, res, next) => {
+
+  let body;
+
+  // const url = "http://l.mat.fyi/rendering_06_light-paths-overview.mp4";
+  const url = "http://10.1.10.17:8000/stream/ignite.mp4";
+  let start = 0;
+
+  console.log(req.headers);
+
+  if (req.headers.range){
+
+    // Break up the range
+    let range = req.headers.range.replace(/bytes=/, '').split("-");
+
+    console.log("RANGE:", range);
+
+    if (range[0]){
+      start = range[0];
+    }
+  }
+
+  const streamReq = http.get(url, {
+    headers: {
+      Range: 'bytes=' + start+ "-",
+    },
+  }, (streamRes) => {
+
+    console.log(streamRes.headers);
+
+    let range = streamRes.headers['content-range'].split('/');
+    
+    const totalSize = range[1];
+
+    // const totalSize = parseInt(streamRes.headers['content-length'], 10);
+    
+    let end = totalSize - 1;
+    
+    console.log("START: ", start);
+    console.log("END: ", end);
+    console.log("TOTAL SIZE: ", totalSize);
+    console.log("TRANSFER SIZE: ", end-start + 1);
+
+    // console.log(start, end, totalSize, end-start + 1);
+    
+    const head = {
+      'Content-Range': 'bytes '+start+'-'+end+'/'+totalSize,
+      'Accept-Ranges': 'bytes',
+      // 'Accept-Ranges': '0-' + end,
+      // 'Content-Length': end-start + 1,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(206, head);
+
+    streamRes.pipe(res);
+
+    // Don't Need This
+    streamRes.on('data', (chunk) => {
+      //console.log("Chunk", chunk);
+      //console.log("Size: ", chunk.byteLength);
+
+      // res.write(chunk);
+    });
+
+    streamRes.on('end', () => {
+      console.log("That's all");
+      //res.end();
+    });
+  });
+
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
